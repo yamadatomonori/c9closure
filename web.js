c7b335e9534e1068ec6d841515c188821ce1f9da
yamadatomonori <tomonori.yamada.g+cloud9@gmail.com>
2012-05-06 14:51:56 +0000

    debug

--- web.js
+++ web.js
@@ -48,7 +48,7 @@ Web.prototype = {
 
     var self = this;
 
-    //command = 'cake';
+    command = 'cake';
     
     require('child_process').exec(command, function(error, stdout, stderr) {
       self.execCallback.call(self, error, stdout, stderr);
